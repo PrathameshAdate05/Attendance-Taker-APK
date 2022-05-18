321ad4bea527751983b06b0ed02ded289345cde6
Prathamesh Adate <prathameshadate05@gmail.com>
2022-05-18 16:00:42 +0530

    Splash Done

--- app/src/main/java/com/prathamesh/attendancetaker/MainActivity.java
+++ app/src/main/java/com/prathamesh/attendancetaker/MainActivity.java
@@ -3,12 +3,20 @@ package com.prathamesh.attendancetaker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView IV_Splash;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        IV_Splash = findViewById(R.id.IV_Splash);
+
+        Glide.with(this).load(R.drawable.checklist).into(IV_Splash);
     }
 }
